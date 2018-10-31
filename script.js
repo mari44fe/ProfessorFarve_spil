98f22d36b3cefeda7864a0fb6a4d7490453a1dfb
mari44fe <mari44fe@stud.kea.dk>
2018-10-31 13:17:45 +0100

    settingmenu mangler tilbage klik , noget af intro skærm færdig

--- script.js
+++ script.js
@@ -21,16 +21,49 @@ function showStart() {
     document.querySelector("#pink_boble").classList.add("fly_up");
     document.querySelector("#pink_boble2").classList.add("fly_up");
 
+    //klik for at spille -introskærm
     document.querySelector("#play_knap").addEventListener("click", hideStart);
+    //klik for at komme til settingsmenu
     document.querySelector("#setting_knap").addEventListener("click", showSettings);
 
 }
 
 function showSettings() {
     console.log("showSettings");
+    //skjul startskærm
+    document.querySelector("#start").classList.remove("show");
+    document.querySelector("#setting_knap").classList.remove("show");
+    //slut animation på kolbe -bobler
+    document.querySelector("#lime_boble").classList.remove("fly_up");
+    document.querySelector("#lime_boble").classList.remove("fly_up");
+    document.querySelector("#lime_boble2").classList.remove("fly_up");
+    document.querySelector("#lime_boble3").classList.remove("fly_up");
+    document.querySelector("#pink_boble").classList.remove("fly_up");
+    document.querySelector("#pink_boble2").classList.remove("fly_up");
+    //vis baggrund
+    document.querySelector("#game_background").classList.add("show");
     //vis settings menu
     document.querySelector("#settings").classList.add("show");
+    //start animation på tilbageknap
+    document.querySelector("#tilbage").classList.add("pulse");
+
+    //klik tilbage til start
+    document.querySelector("#tilbage").addEventListener("click", hideSettings);
 }
+
+function hideSettings() {
+    //skjul baggrund
+    document.querySelector("#game_background").classList.remove("show");
+    //skjul settings menu
+    document.querySelector("#settings").classList.remove("show");
+    //stop animation på tilbageknap
+    document.querySelector("#tilbage").classList.remove("pulse");
+}
+
+// når fade-animationen er færdig -> startGame
+
+//document.querySelector("#settings").addEventListener("animationend", showStart);
+
 
 function hideStart() {
     console.log("hideStart");
@@ -54,9 +87,17 @@ function startGame() {
     console.log("startGame");
     //sjul startskærm
     document.querySelector("#start").classList.add("hide");
+    //vis baggrund
+    document.querySelector("#game_background").classList.add("show");
     //vis spilskærm
     document.querySelector("#game_foreground_intro").classList.add("show");
     document.querySelector("#intro_tekst").classList.add("show");
-    document.querySelector("#frem").classList.add("pulse");
+
+    //start animation på taleboble -fortæller error
+    //document.querySelector("#intro_tekst").classList.add("fade_in");
+    document.querySelector("#frem").classList.add("show");
+
+    //start animation på fremknap -vil ikke forblive roteret
+    //document.querySelector("#frem").classList.add("pulse");
 
 }
